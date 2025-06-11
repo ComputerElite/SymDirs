c41bacb35d8ebc3b95bfcc1d0444d3e47040d3b3
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2025-06-11 22:03:04 +0200

    Add gitignore

---
+++ SymDirs/Migrations/20250611192934_initial_scheme.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SymDirs.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_scheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DbFile",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    FullPath = table.Column<string>(type: "TEXT", nullable: false),
+                    Hash = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastSync = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    State = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DbFile", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DbFile_FullPath",
+                table: "DbFile",
+                column: "FullPath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DbFile");
+        }
+    }
+}
